ba7478fc4143a5b5993dcdfeec788c4fc3cb8d02
Kris Steinhoff <kris@ofa2.com>
2017-12-27 20:55:45 -0500

    Prepare 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dbcl',
-    version='0.1.0',
+    version='0.1.1',
     description='A database command line interface that is engine-agnostic.',
     author='Kris Steinhoff',
     url='https://github.com/ksofa2/dbcl',
