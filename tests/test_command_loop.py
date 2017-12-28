f8f4c3a3f015095a0e765b7a2e10d640f1334558
Kris Steinhoff <kris@ofa2.com>
2017-12-28 15:05:12 -0500

    Add tests

---
+++ tests/test_command_loop.py
@@ -0,0 +1,34 @@
+import pytest
+
+from dbcl.command_line import command_loop
+
+
+@pytest.fixture()
+def mock_get_args(mocker):
+    return mocker.patch('dbcl.command_line.get_args')
+
+
+@pytest.fixture()
+def mock_get_engine(mocker):
+    return mocker.patch('dbcl.command_line.get_engine')
+
+
+@pytest.fixture()
+def mock_in_memory_history(mocker):
+    return mocker.patch('dbcl.command_line.InMemoryHistory')
+
+
+def test_normal_exit(mock_get_args, mock_get_engine, mock_in_memory_history,
+                     mocker):
+    mock_prompt = mocker.patch(
+        'dbcl.command_line.prompt',
+        side_effect=EOFError)
+    with pytest.raises(SystemExit) as excinfo:
+        command_loop()
+
+    assert excinfo.value.code == 0
+
+    assert mock_prompt.called
+    assert mock_get_args.called
+    assert mock_get_engine.called
+    assert mock_in_memory_history.called
--- tests/test_prompt_for_command.py
+++ tests/test_prompt_for_command.py
@@ -1,9 +1,66 @@
+import pytest
+
 from dbcl.command_line import prompt_for_command
 
 
-def test_prompt(mocker):
+def test_prompt_with_command(mocker):
+    mock_process_command = mocker.patch('dbcl.command_line.process_comand')
+    mock_get_engine = mocker.patch('dbcl.command_line.get_engine')
+    mock_print_result = mocker.patch('dbcl.command_line.print_result')
     mock_prompt = mocker.patch('dbcl.command_line.prompt')
+    mock_prompt.return_value = '--/foo'
 
-    prompt_for_command(None)
+    prompt_for_command(None, mock_get_engine.return_value, None)
 
+    assert not mock_get_engine.return_value.called
+    assert mock_prompt.called
+    assert not mock_print_result.called
+    assert mock_process_command.called
+
+
+def test_prompt_with_query(mocker):
+    mock_process_command = mocker.patch('dbcl.command_line.process_comand')
+    mock_get_engine = mocker.patch('dbcl.command_line.get_engine')
+    mock_print_result = mocker.patch('dbcl.command_line.print_result')
+    mock_prompt = mocker.patch('dbcl.command_line.prompt')
+    mock_prompt.return_value = 'foo'
+
+    prompt_for_command(None, mock_get_engine.return_value, None)
+
+    assert mock_get_engine.return_value.execute.called
+    assert mock_prompt.called
+    assert mock_print_result.called
+    assert not mock_process_command.called
+
+
+def test_user_interupt(mocker):
+    mock_prompt = mocker.patch('dbcl.command_line.prompt',
+                               side_effect=KeyboardInterrupt)
+
+    cmd = prompt_for_command(None, None, None)
+
+    assert cmd is None
+    assert mock_prompt.called
+
+
+def test_user_eof(mocker):
+    mock_prompt = mocker.patch('dbcl.command_line.prompt',
+                               side_effect=EOFError)
+
+    with pytest.raises(SystemExit) as excinfo:
+        prompt_for_command(None, None, None)
+
+    assert excinfo.value.code == 0
+    assert mock_prompt.called
+
+
+def test_user_other_error(mocker, capsys):
+    mock_prompt = mocker.patch('dbcl.command_line.prompt',
+                               side_effect=Exception('test error'))
+
+    cmd = prompt_for_command(None, None, None)
+
+    out, err = capsys.readouterr()
+    assert 'test error' in out
+    assert cmd is None
     assert mock_prompt.called
