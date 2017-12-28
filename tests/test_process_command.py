70587630447259f8e7f69b718b0f1f71fb88a7d2
Kris Steinhoff <kris@ofa2.com>
2017-12-28 13:51:48 -0500

    Add tests for process_command

--- tests/test_process_command.py
+++ tests/test_process_command.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dbcl.command_line import process_comand, _command_prefix
+from dbcl.command_line import process_comand, _command_prefix, NoSuchTableError
 
 
 @pytest.fixture(autouse=True)
@@ -23,15 +23,56 @@ info_too_many = ['%s%s' % (_command_prefix, cmd) for cmd in
                  ['info too many', 'info way too many args']]
 
 
+def test_info_no_args(mocker, capsys):
+    mock_args = mocker.MagicMock()
+    mock_args.database_url = 'test_db_url'
+    mock_metadata = mocker.patch('dbcl.command_line.MetaData')
+
+    process_comand('%sinfo' % _command_prefix, None, mock_args)
+
+    assert mock_metadata.return_value.reflect.called
+    out, err = capsys.readouterr()
+    assert 'Database URL' in out
+    assert 'Tables' in out
+
+
+def test_info_one_arg(mocker, capsys):
+    mock_args = mocker.MagicMock()
+    mock_args.database_url = 'test_db_url'
+    mock_table = mocker.patch('dbcl.command_line.Table')
+    mock_print_data = mocker.patch('dbcl.command_line.print_data')
+
+    process_comand('%sinfo table_name' % _command_prefix, None, mock_args)
+
+    assert mock_table.called
+    assert mock_print_data.called
+
+
+def test_info_missing_table(mocker, capsys):
+    mock_args = mocker.MagicMock()
+    mock_args.database_url = 'test_db_url'
+    mock_table = mocker.patch('dbcl.command_line.Table',
+                              side_effect=NoSuchTableError)
+    mock_print_data = mocker.patch('dbcl.command_line.print_data')
+
+    process_comand('%sinfo table_name' % _command_prefix, None, mock_args)
+
+    assert mock_table.called
+    assert not mock_print_data.called
+    out, err = capsys.readouterr()
+    assert out.startswith('No such table')
+
+
 @pytest.mark.parametrize('command', (
     '%s%s' % (_command_prefix, cmd) for cmd in
     ('info too many', 'info way too many args')))
-def test_info_too_many(command, capsys):
+def test_info_too_many_args(command, capsys):
 
     process_comand(command, None, None)
 
     out, err = capsys.readouterr()
     assert out.startswith('usage: ')
+
 
 #
 # def test_with_url_on_command_line(mocker):
